c5a683f098657d8da2bab9ce43d98c95fe98d70b
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2024-07-02 18:08:11 -0600

    Update TGAHeader.java

--- TGAHeader.java
+++ TGAHeader.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package jendgamexbox;
 
 import java.nio.ByteBuffer;
@@ -9,7 +6,7 @@ import java.nio.ByteOrder;
 
 /**
  *
- * @author localadmin
+ * @author Slam
  */
 public class TGAHeader {
 
