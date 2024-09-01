8910b1cd5733adeb0dcadf0b79f3e6781f9a4083
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2024-09-01 17:56:05 -0600

    Update TGAHeader.java

--- TGAHeader.java
+++ TGAHeader.java
@@ -42,21 +42,21 @@ public class TGAHeader {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("TGAHeader - \n");
-        sb.append("            id_len: 0x").append(String.format("%02X", idLen)).append("\n")
-                .append("      color_map_type: 0x").append(String.format("%02X", colorMapType)).append("\n")
-                .append("           img_type: 0x").append(String.format("%02X", imgType)).append("\n")
-                .append("     color_map_ofs: 0x").append(String.format("%02X", colorMapOfs)).append("\n")
-                .append("      num_color_map: 0x").append(String.format("%02X", numColorMap)).append("\n")
-                .append("    color_map_depth: 0x").append(String.format("%02X", colorMapDepth)).append("\n")
-                .append("           x_offset: 0x").append(String.format("%02X", xOffset)).append("\n")
-                .append("           y_offset: 0x").append(String.format("%02X", yOffset)).append("\n")
-                .append("             width: 0x").append(String.format("%02X", width)).append("\n")
-                .append("            height: 0x").append(String.format("%02X", height)).append("\n")
-                .append("          img_depth: 0x").append(String.format("%02X", imgDepth)).append("\n")
-                .append("      img_descriptor: 0x").append(String.format("%02X", imgDescriptor)).append("\n")
-                .append("      |- top_to_bottom: ").append(isTopToBottom()).append("\n")
-                .append("      |- left_to_right: ").append(isLeftToRight()).append("\n")
-                .append("      |--- compressed: ").append(isCompressed()).append("\n");
+        sb.append(" id_len: 0x").append(String.format("%02X", idLen)).append("\n")
+        .append(" color_map_type: 0x").append(String.format("%02X", colorMapType)).append("\n")
+        .append(" img_type: 0x").append(String.format("%02X", imgType)).append("\n")
+        .append(" color_map_ofs: 0x").append(String.format("%02X", colorMapOfs)).append("\n")
+        .append(" num_color_map: 0x").append(String.format("%02X", numColorMap)).append("\n")
+        .append(" color_map_depth: 0x").append(String.format("%02X", colorMapDepth)).append("\n")
+        .append(" x_offset: 0x").append(String.format("%02X", xOffset)).append("\n")
+        .append(" y_offset: 0x").append(String.format("%02X", yOffset)).append("\n")
+        .append(" width: 0x").append(String.format("%02X", width)).append("\n")
+        .append(" height: 0x").append(String.format("%02X", height)).append("\n")
+        .append(" img_depth: 0x").append(String.format("%02X", imgDepth)).append("\n")
+        .append(" img_descriptor: 0x").append(String.format("%02X", imgDescriptor)).append("\n")
+        .append(" |- top_to_bottom: ").append(isTopToBottom()).append("\n")
+        .append(" |- left_to_right: ").append(isLeftToRight()).append("\n")
+        .append(" |--- compressed: ").append(isCompressed()).append("\n");
         return sb.toString();
     }
 
